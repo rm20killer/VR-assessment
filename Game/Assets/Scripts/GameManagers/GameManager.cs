29396057f989881f5bfc9c42e1a972543a8074aa
RM20 <43253883+rm20killer@users.noreply.github.com>
2024-03-08 12:40:11 +0000

    work on assessment start

---
+++ Game/Assets/Scripts/GameManagers/GameManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public int[] Key;
+    void Start()
+    {
+        //generate a random number between 1 and 9 and store it in the variable key
+        Key = new int[4];
+        for (int i = 0; i < 4; i++)
+        {
+            Key[i] = Random.Range(1, 9);
+        }
+    }
+
+}
---
+++ Game/Assets/Scripts/GameManagers/Keys/key.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameManagers.Keys
+{
+    public class key
+    {
+        public int KeyID;
+        public int[] KeyCode;
+
+        void Setkey()
+        {
+            //generate a random number between 1 and 9 and store it in the variable key
+            KeyCode = new int[4];
+            for (int i = 0; i < 4; i++)
+            {
+                KeyCode[i] = Random.Range(1, 9);
+            }
+        }
+        
+        void SetKeyID(int id)
+        {
+            //set the key id
+            KeyID = id;
+        }
+        
+        void setKey(int[] key)
+        {
+            //set the keycode
+            KeyCode = key;
+        }
+        int[] GetKey()
+        {
+            //return the keycode
+            return KeyCode;
+        }
+        
+        int GetKeyID()
+        {
+            //return the key id
+            return KeyID;
+        }
+        
+    }
+}
